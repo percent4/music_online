ae1d33797efc82474b9a6a04c779afca686f4709
Jclian <33828982+percent4@users.noreply.github.com>
2018-07-13 14:21:21 +0800

    增加搜索历史功能

--- music/src/main/java/com/hello/music/Contoller/SongController.java
+++ music/src/main/java/com/hello/music/Contoller/SongController.java
@@ -5,29 +5,56 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Controller
 
 public class SongController {
 
     @GetMapping("/index")
-    public String getForm(Model model) {
+    public String getForm(Model model, Map <String, Object> map){
+
         model.addAttribute("songInfo", new Song_Info());
+        // 获取访问量信息
+        String txtFilePath = "/home/vcap/app/BOOT-INF/classes/static/others/count.txt";
+        Long count = Get_Visit_Count(txtFilePath);
+        System.out.println(count);
+        map.put("count", count);
+
         return "index";
     }
 
     @PostMapping("/index")
     public String submitForm(@ModelAttribute Song_Info songInfo, Map <String, Object> map) throws Exception {
+
+        // 获取歌曲信息
         String song_name = songInfo.getSong();
         String platform = songInfo.getPlatform();
         System.out.println(song_name+':'+platform);
+        writeTXT(song_name,platform); // 写入到history.csv文件中，用来显示搜索历史
         MusicDownload music_download = new MusicDownload(song_name, platform);
-        byte[] post_data = music_download.sendPost().getBytes("utf-8");
+        byte[] post_data = music_download.doPost().getBytes("utf-8");
         String info = new String(post_data,"utf-8");
         System.out.println(info);
         map.put("music", info);
+
+        ArrayList<String> records = read_txt();
+        ArrayList<String> columns = new ArrayList<>();
+        for(int i=0;i<records.size();i++){
+            columns.add(records.get(i));
+        }
+        map.put("columns", columns);
+        for(String column: columns){
+            System.out.println(column);
+        }
 
         return "play";
     }
@@ -37,6 +64,185 @@ public class SongController {
         return "dev";
     }
 
+    //文件下载相关代码
+    @RequestMapping("/download")
+    public String downloadFile(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+        //String fileName = "aim_test.txt";  //下载的文件名
+        File scFileDir = new File("/home/vcap/app/BOOT-INF/classes/static/music_store");
+        File TrxFiles[] = scFileDir.listFiles();
+        System.out.println(TrxFiles[0]);
+        String fileName = TrxFiles[0].getName(); //下载的文件名
+        if (fileName != null) {
+            //设置文件路径
+            String realPath = "/home/vcap/app/BOOT-INF/classes/static/music_store/";
+            File file = new File(realPath, fileName);
+            if (file.exists()) {
+                response.setHeader("content-type", "application/octet-stream");
+                response.setContentType("application/octet-stream");
+                response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    System.out.println("Download the song successfully!");
+                } catch (Exception e) {
+                    System.out.println("Download the song failed!");
+                } finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
 
+    public static Long Get_Visit_Count(String txtFilePath) {
+
+        try {
+            //读取文件(字符流)
+            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(txtFilePath),"UTF-8"));
+            //循环读取数据
+            String str = null;
+            StringBuffer content = new StringBuffer();
+            while ((str = in.readLine()) != null) {
+                content.append(str);
+            }
+            //关闭流
+            in.close();
+
+            //System.out.println(content);
+            // 解析获取的数据
+            Long count = Long.valueOf(content.toString());
+            count ++;
+            //写入相应的文件
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(txtFilePath),"UTF-8"));
+            out.write(String.valueOf(count));
+
+            //清楚缓存
+            out.flush();
+            //关闭流
+            out.close();
+
+            return count;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return 0L;
+        }
+    }
+
+    public static void writeTXT(String song_name, String platform) {
+
+        String filePath = "/home/vcap/app/BOOT-INF/classes/static/others/history.txt";
+        //String filePath = "E://history.txt";
+
+        FileWriter writer = null;
+        try {
+            // 打开一个写文件器，构造函数中的第二个参数true表示以追加形式写文件
+            writer = new FileWriter(filePath, true);
+            String Platform = null;
+            if(platform.equals("qq")){
+                Platform = "QQ";
+            }
+            if(platform.equals("netease")){
+                Platform = "网易";
+            }
+            if(platform.equals("kuwo")){
+                Platform = "酷我";
+            }
+
+            Date now = new Date();
+            SimpleDateFormat bjSdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");     // 北京
+            bjSdf.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+
+            byte[] write_content = new String(song_name+','+Platform+','+bjSdf.format(now)+'\n').getBytes("UTF-8");
+            writer.write(new String(write_content,"UTF-8"));
+            System.out.println("Write to history.txt successfully!");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if(writer != null){
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static ArrayList<String> read_txt() {
+        String filePath = "/home/vcap/app/BOOT-INF/classes/static/others/history.txt";
+        //String filePath = "E://history.txt";
+
+        File file = new File(filePath);
+        BufferedReader reader = null;  // 设置reader为null
+        ArrayList<String> content = new ArrayList<String>();
+
+        try {
+
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null)
+                content.add(tempString); // 在列表中添加国家名称
+
+            reader.close(); // 关闭reader
+
+            if(content.size() < 10) {
+                return content;
+            }
+            else{
+                ArrayList<String> result = new ArrayList<>();
+                int length = content.size();
+                int k = length;
+                while(k >= length-9){
+                    result.add(content.get(k-1));
+                    k --;
+                }
+
+                return result;
+            }
+        }
+        catch (IOException e) {
+            return content;
+        }
+        finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                }
+                catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+
+            }
+
+        }
+
+    }
 
 }
